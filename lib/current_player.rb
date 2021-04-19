604d9fb013905484822ec74109b76020ecb81c16
Rishav Maskey <rishav.maskey@hotmail.com>
2021-04-19 10:16:08 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,6 +2,6 @@ def turn_count board
   board.select {|v| v == "X" || v == "O"}.length
 end
 
-def current_player
-  turn_count %  2 == 0 ? "O" : "X"
+def current_player board
+  turn_count board %  2 == 0 ? "O" : "X"
 end
